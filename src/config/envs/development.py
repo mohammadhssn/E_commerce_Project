d8dac876b7b8b7901ec54cbfe3041673edcd2f71
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2022-03-12 19:27:12 +0330

    added package sentry for log error and update requirements.txt

--- src/config/envs/development.py
+++ src/config/envs/development.py
@@ -1,4 +1,6 @@
 from ..settings import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # debug_toolbar settings
 if DEBUG:
@@ -26,9 +28,57 @@ if DEBUG:
         "INTERCEPT_REDIRECTS": False,
     }
 
+# Config redis cache
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379',
     }
 }
+
+# Config Log
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        },
+        'file': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': '/tmp/debug.log'
+        }
+    },
+    'loggers': {
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+        }
+    }
+}
+
+# Config Sentry
+sentry_sdk.init(
+    dsn="https://fa805be7a1eb43e082be46764c45e7e3@o1133698.ingest.sentry.io/6256089",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
